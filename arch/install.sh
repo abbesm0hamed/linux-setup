cfa0e22a14fce54b49bd7bc2941805459929ca46
abbesm0hamed <abbesmohamed717@gmail.com>
2024-10-31 16:26:40 +0100

    feat(linux func): touchpad touch working now...

--- arch/install.sh
+++ arch/install.sh
@@ -12,7 +12,6 @@ sudo pacman -S ansible --noconfirm
 # verify ansible installation
 echo "Verifying ansible installation..."
 ansible --version
-
 echo "Ansible installation complete."
 
 # install yay
@@ -21,5 +20,19 @@ sudo pacman -S --needed git base-devel --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-
+cd ..
 echo "yay installation complete."
+
+# configure touchpad tapping
+echo "Configuring touchpad for tap-to-click..."
+sudo pacman -S xf86-input-libinput --noconfirm
+sudo mkdir -p /etc/X11/xorg.conf.d/
+echo 'Section "InputClass"
+    Identifier "touchpad"
+    MatchIsTouchpad "on"
+    Driver "libinput"
+    Option "Tapping" "on"
+    Option "ClickMethod" "clickfinger"
+EndSection' | sudo tee /etc/X11/xorg.conf.d/40-libinput.conf > /dev/null
+
+echo "Touchpad configuration complete. Please restart the display manager or reboot for changes to take effect."
